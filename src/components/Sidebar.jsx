caa4a99a4180f122ca698ca5fd42c7d54da8230e
Dolly Chauhan <abcddolly1234@gmail.com>
2023-01-22 22:39:22 +0530

    API integration with categories

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { Stack } from '@mui/material';
 import { categories } from '../utilities/constants';
 
-const selectedCategory = "New";
-const Sidebar = () => (
+
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => (
 
     <Stack
         direction="row"
@@ -14,10 +14,12 @@ const Sidebar = () => (
         }}
     >
         {categories.map((category) => (
-            <button className="category-btn" style={{
-                background: category.name === selectedCategory && "#FC1503",
-                color: "white"
-            }}
+            <button className="category-btn"
+                onClick={() => setSelectedCategory(category.name)}
+                style={{
+                    background: category.name === selectedCategory && "#FC1503",
+                    color: "white"
+                }}
                 key={category.name}
             >
                 <span style={{
---
+++ src/utilities/fetchfromAPI.js
@@ -0,0 +1,21 @@
+
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    }
+};
+
+export const fetchfromAPI = async (url) => {
+    let data = await fetch(`${BASE_URL}/${url}`, options);
+
+    data = await data.json();
+    console.log(data);
+
+    return data;
+
+}
