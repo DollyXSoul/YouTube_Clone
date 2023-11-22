c1676c1fca578575a39c9b1d5f47046173aec172
Dolly <abcddolly1234@gmail.com>
2023-11-22 14:01:13 +0530

    resolving file-naming issues,dependency conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
@@ -9,14 +10,15 @@ import {
   ChannelDetail,
 } from "./components";
 import ScrollToTop from "./utilities/ScrollToTop";
+import { ReactElement } from "react";
 
-const App = () => (
+const App: React.FC = () => (
   <BrowserRouter>
     <ScrollToTop />
     <Box sx={{ backgroundColor: "#000" }}>
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Feed />} />
+        <Route path="/" element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
         <Route path="/channel/:id" element={<ChannelDetail />} />
         <Route path="/search/:searchTerm" element={<SearchFeed />} />
